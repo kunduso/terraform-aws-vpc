0b6a6807cf3eebba7474496e347d84bfb377b41b
Sourav Kundu <skundu.dev@gmail.com>
2024-11-11 21:21:54 -0600

    added condition for vpc

--- main.tf
+++ main.tf
@@ -1,27 +1,87 @@
-resource "aws_vpc" "myvpc" {
-  cidr_block           = var.network_address_space
-  enable_dns_hostnames = "true"
+# https://docs.aws.amazon.com/glue/latest/dg/set-up-vpc-dns.html
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_support
+  enable_dns_support = var.enable_dns_support == null ? false : var.enable_dns_support
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_hostnames
+  enable_dns_hostnames = var.enable_dns_hostnames == null ? false : var.enable_dns_hostnames
+  #checkov:skip=CKV2_AWS_11: Not creating a flow log for this VPC
   tags = {
-    Name        = "ADO-VPC-Int"
-    Environment = "Dev"
+    "Name" = var.vpc_name == "" ? "vpc-${random_string.vpc_name[0].result}" : var.vpc_name
   }
 }
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/availability_zones
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-resource "aws_subnet" "mysubnet1" {
-  cidr_block              = var.subnet1_address_space
-  vpc_id                  = aws_vpc.myvpc.id
-  map_public_ip_on_launch = "true"
-  availability_zone       = data.aws_availability_zones.available.names[0]
-}
-
-resource "aws_subnet" "mysubnet2" {
-  cidr_block              = var.subnet2_address_space
-  vpc_id                  = aws_vpc.myvpc.id
-  map_public_ip_on_launch = "true"
-  availability_zone       = data.aws_availability_zones.available.names[1]
-}
+#  data "aws_availability_zones" "available" {
+#   state = "available"
+# }
+# resource "aws_subnet" "private" {
+#   count             = length(var.subnet_cidr_private)
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = var.subnet_cidr_private[count.index]
+#   availability_zone = data.aws_availability_zones.available.names[count.index]
+#   tags = {
+#     "Name" = "app-2-private-${count.index + 1}"
+#   }
+# }
+# resource "aws_subnet" "public" {
+#   count             = length(var.subnet_cidr_public)
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = var.subnet_cidr_public[count.index]
+#   availability_zone = data.aws_availability_zones.available.names[count.index]
+#   tags = {
+#     "Name" = "app-2-public-${count.index + 1}"
+#   }
+# }
+# resource "aws_route_table" "private" {
+#   count  = length(var.subnet_cidr_private)
+#   vpc_id = aws_vpc.this.id
+#   tags = {
+#     "Name" = "app-2-route-table-${count.index + 1}"
+#   }
+# }
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.this.id
+#   tags = {
+#     "Name" = "app-2-public"
+#   }
+# }
+# resource "aws_route_table_association" "private" {
+#   count          = length(var.subnet_cidr_private)
+#   subnet_id      = element(aws_subnet.private.*.id, count.index)
+#   route_table_id = aws_route_table.private[count.index].id
+# }
+# resource "aws_route_table_association" "public" {
+#   count          = length(var.subnet_cidr_public)
+#   subnet_id      = element(aws_subnet.public.*.id, count.index)
+#   route_table_id = aws_route_table.public.id
+# }
+# # resource "aws_internet_gateway" "this-igw" {
+# #   vpc_id = aws_vpc.this.id
+# #   tags = {
+# #     "Name" = "app-2-gateway"
+# #   }
+# # }
+# # resource "aws_route" "internet-route" {
+# #   destination_cidr_block = "0.0.0.0/0"
+# #   route_table_id         = aws_route_table.public.id
+# #   gateway_id             = aws_internet_gateway.this-igw.id
+# # }
+# # resource "aws_eip" "nat_gateway" {
+# #   count  = length(var.subnet_cidr_public)
+# #   domain = "vpc"
+# #   #checkov:skip=CKV2_AWS_19: The IP is attached to the NAT gateway
+# # }
+# # resource "aws_nat_gateway" "public" {
+# #   count         = length(var.subnet_cidr_public)
+# #   subnet_id     = element(aws_subnet.public.*.id, count.index)
+# #   allocation_id = aws_eip.nat_gateway[count.index].id
+# #   depends_on    = [aws_internet_gateway.this-igw]
+# #   tags = {
+# #     "Name" = "app-2-NAT-${count.index + 1}"
+# #   }
+# # }
+# # resource "aws_route" "private-route" {
+# #   count                  = length(var.subnet_cidr_private)
+# #   destination_cidr_block = "0.0.0.0/0"
+# #   route_table_id         = aws_route_table.private[count.index].id
+# #   nat_gateway_id         = aws_nat_gateway.public[count.index].id
+# # }
---
+++ random.tf
@@ -0,0 +1,7 @@
+#https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/auth.html#auth-overview
+#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password
+resource "random_string" "vpc_name" {
+  count   = var.vpc_name == "" ? 1 : 0
+  length  = 5
+  special = false
+}
