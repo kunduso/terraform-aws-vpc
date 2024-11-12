8254c245876be9564c5a279c4216ddc589e50d96
Sourav Kundu <skundu.dev@gmail.com>
2024-11-11 21:21:25 -0600

    updated provider minimum version

--- provider.tf
+++ provider.tf
@@ -2,13 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.29.0"
+      version = ">= 5.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
     }
   }
 }
 
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+# provider "aws" {
+#   region     = var.region
+#   access_key = var.access_key
+#   secret_key = var.secret_key
+# }
+
+# provider "random" {}
