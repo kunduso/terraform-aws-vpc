f472d32e151bdf6d5399d621797f12c4614ff957
Sourav Kundu <skundu.dev@gmail.com>
2024-11-11 21:22:33 -0600

    updated variables

--- variable.tf
+++ variable.tf
@@ -1,23 +1,41 @@
 variable "region" {
-  description = "The region where to provision resources"
+  description = "The AWS region to provision resources."
   type        = string
+  default     = "us-east-1"
 }
-variable "network_address_space" {
-  default = "10.1.0.0/16"
+# variable "access_key" {
+#   description = "The IAM access_key."
+#   type        = string
+#   sensitive   = true
+# }
+# variable "secret_key" {
+#   description = "The IAM secret_key."
+#   type        = string
+#   sensitive   = true
+# }
+variable "vpc_cidr" {
+  description = "CIDR for the VPC."
+  default     = "10.20.30.0/24"
 }
-variable "access_key" {
-  description = "The access_key that belongs to the IAM user"
-  type        = string
-  sensitive   = true
+variable "subnet_cidr_private" {
+  description = "CIDR blocks for the private subnets."
+  default     = ["10.20.30.0/27", "10.20.30.32/27", "10.20.30.64/27"]
+  type        = list(any)
 }
-variable "secret_key" {
-  description = "The secret_key that belongs to the IAM user"
-  type        = string
-  sensitive   = true
+variable "subnet_cidr_public" {
+  description = "CIDR blocks for the public subnets."
+  default     = ["10.20.30.96/27"]
+  type        = list(any)
 }
-variable "subnet1_address_space" {
-  default = "10.1.0.0/24"
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames for VPC."
+  default     = false
 }
-variable "subnet2_address_space" {
-  default = "10.1.1.0/24"
+variable "enable_dns_support" {
+  description = "Enable DNS support for VPC."
+  default     = false
+}
+variable "vpc_name" {
+  description = "Name of the VPC."
+  default     = ""
 }
