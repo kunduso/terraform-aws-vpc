5ac4cae510154992ec5224f1a3951e067e561a9d
Sourav Kundu <skundu.dev@gmail.com>
2024-11-15 20:00:49 -0600

    added resource tag

--- security_group.tf
+++ security_group.tf
@@ -1,4 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/default_security_group
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.this.id
+  tags = {
+    "Name" = "${local.vpc_name}-default"
+  }
 }
