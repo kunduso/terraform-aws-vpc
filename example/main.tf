a03e23085ebad23803e1114220ce5a4586c973e4
Sourav Kundu <skundu.dev@gmail.com>
2025-01-09 22:29:19 -0600

    fix for checkov scan

--- example/main.tf
+++ example/main.tf
@@ -12,4 +12,6 @@ module "vpc" {
     Environment    = "dev"
     Source         = "https://github.com/kunduso/terraform-aws-vpc"
   }
+  #CKV_TF_1: Ensure Terraform module sources use a commit hash
+  #checkov:skip=CKV_TF_1: This is a self hosted module where the version number is tagged rather than the commit hash.
 }
