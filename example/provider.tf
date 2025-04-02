f14abd213dbddfb3b0af584a9b5b4c8b36d413af
Sourav Kundu <skundu.dev@gmail.com>
2025-04-02 16:18:26 -0500

    added default tag reference

--- example/provider.tf
+++ example/provider.tf
@@ -15,4 +15,9 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+  default_tags {
+    tags = {
+      Source = "https://github.com/kunduso/terraform-aws-vpc/example"
+    }
+  }
 }
